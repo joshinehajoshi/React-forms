1ea29aceca1e7acae8b8360e719aa79a925fc6f1
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-06-04 09:07:37 +0530

    submitForm.css

---
+++ src/components/SubmitForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import '../style/submitForm.css';
+
+
+function SubmitForm() {
+  return (
+    <div className='form-wrapper'>
+            <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        </Form>
+    </div>
+  )
+}
+
+export default SubmitForm
